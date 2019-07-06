fe89ba0ec151f2ac1bb614361da75b5f9b25bd72
arriival <g9266312114>
2019-07-06 22:42:42 +0430

    modified ActionController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,12 @@
                 </div>
             </div>
             <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
                 @yield('cardBody')
             </div>
             <!-- /.card-body -->
